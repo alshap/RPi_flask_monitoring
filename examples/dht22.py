5add9f97347197b40b5cb2a3c25d77ba8c5641bc
alshap <40171499+alshap@users.noreply.github.com>
2020-07-16 14:12:15 +0100

    Add files via upload
    Timeout added

--- examples/dht22.py
+++ examples/dht22.py
@@ -1,16 +1,27 @@
+import timeout_decorator
 import Adafruit_DHT
+import time
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
+@timeout_decorator.timeout(5)
 def main():
-    while True:
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    try:
+        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
         if humidity is not None and temperature is not None:
             print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
         else:
             print("Failed to retrieve data from humidity sensor")
+    except timeout_decorator.timeout_decorator.TimeoutError:
+        print("Timeout exceeded. Retrying")
             
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
+        time.sleep(1)
+
+    
+    
+
