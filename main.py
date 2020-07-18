f7ae98527e425478bffc30b438186b1c6029d9f1
alshap <40171499+alshap@users.noreply.github.com>
2020-07-19 00:17:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,21 @@
+import sys
+sys.path.append("./classes")
+
 import timeout_decorator
-from DataCollect import DataCollect
+import threading
 import time
 import psycopg2
 from config import config
 import datetime
+from SensorList import SensorList
+from DeviceList import DeviceList
 
-postgres_query = """ INSERT INTO sensordata (sensor_id, data, time, value_type) VALUES (%s, %s, %s, %s)"""
-
-
-data_collection = DataCollect(DHT_PIN = 4, ECHO_PIN = 17, TRIG_PIN = 27, RESISTOR_PIN = 22)
-data_collection.setup()
+postgres_query = """ INSERT INTO sensordata (sensor_id, data, time) VALUES (%s, ARRAY [%s], %s)"""
 
 def main():
-    distance = ultrasonic_value()
-    temperature, humidity = temperature_humidity_value()
-    light = photoresistor_value()
-    if temperature is not None and humidity is not None:
-        postgre_insert(postgres_query, [1, temperature, datetime.datetime.now(), 'celsius'])
-        postgre_insert(postgres_query, [1, humidity, datetime.datetime.now(), 'percent'])
-    if distance is not None:
-        postgre_insert(postgres_query, [2, distance, datetime.datetime.now(), 'cm'])
-    if light is not None:
-        postgre_insert(postgres_query, [3, light, datetime.datetime.now(), 'bool'])
-
-@timeout_decorator.timeout(5)
-def ultrasonic_value():
-    try:
-        distance = data_collection.readUltrasonicDistance()
-        return distance
-    except timeout_decorator.timeout_decorator.TimeoutError:
-        print("Timeout exceeded. Retrying")
-
-@timeout_decorator.timeout(5)
-def temperature_humidity_value():
-    try:
-        temperature, humidity = data_collection.readTemperatureHumidity()
-        return temperature, humidity
-    except timeout_decorator.timeout_decorator.TimeoutError:
-        print("Timeout exceeded. Retrying")
-
-@timeout_decorator.timeout(5)
-def photoresistor_value():
-    try:
-        light = data_collection.readPhotoresistorValue()
-        return light
-    except timeout_decorator.timeout_decorator.TimeoutError:
-        print("Timeout exceeded. Retrying")
-        
+    data_thread = threading.Thread(target=thread_getSendData, args=())
+    data_thread.start()
+    
 def postgre_insert(query, record):
     connection = False
     try:
@@ -62,7 +31,21 @@ def postgre_insert(query, record):
         if(connection):
             cursor.close()
             connection.close()
-            
-if __name__ == '__main__':
+
+def thread_getSendData():
+    sensorList = SensorList()
+    sensorList.setup()
+    print(sensorList.message())
+
+    sensors = sensorList.sensors
+    
     while True:
-        main()
+        for sensor in sensors:
+            sensor.message()
+            values = sensor.readValues()
+            if str(values)[0] is not None:
+                postgre_insert(postgres_query, [sensor.sensor_id, values, datetime.datetime.now()])
+        time.sleep(20)
+    
+if __name__ == '__main__':
+    main()
