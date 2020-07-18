07aca759851a5eb793a6f3934c6d6376b770c9ea
alshap <40171499+alshap@users.noreply.github.com>
2020-07-19 00:19:41 +0300

    Tables structure changed

--- db/dump.sql
+++ db/dump.sql
@@ -1,11 +1,9 @@
-create database flask;
-
-create table sensor (sensor_id serial, name text, pin integer, description text null, primary key(sensor_id));
-create table sensordata (id serial, sensor_id integer, data float, time timestamp, value_type integer, constraint fk_sensor foreign key(sensor_id) references sensor(sensor_id));
+create table sensor (sensor_id serial, name text, pin integer, description text null, value_type text [], primary key(sensor_id));
+create table sensordata (id serial, sensor_id integer, data float [], time timestamp, constraint fk_sensor foreign key(sensor_id) references sensor(sensor_id));
 create table device (device_id serial, name text, pin integer, description text null, primary key(device_id));
 
-insert into sensor(name, pin) values ('dht22', 4);
-insert into sensor(name, pin) values ('ultrasonic', 17);
-insert into sensor(name, pin) values ('resistor', 22);
+insert into sensor(name, pin, value_type) values ('DHT22', 4, ARRAY ['C', '%']);
+insert into sensor(name, pin, value_type) values ('ultrasonic', 17, ARRAY ['cm']);
+insert into sensor(name, pin, value_type) values ('Photoresistor', 22, ARRAY ['boolean']);
 
 insert into device(name, pin) values ('red_led', 26);
