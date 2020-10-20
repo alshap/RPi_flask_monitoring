c52ebdc1f6c649bd0a4e59d084dd49d6f7d05e92
alshap <40171499+alshap@users.noreply.github.com>
2020-10-20 13:39:05 +0300

    Adapted for test values

--- db/dump.sql
+++ db/dump.sql
@@ -2,8 +2,7 @@ create table sensor (sensor_id serial, name text, pin integer, description text 
 create table sensordata (id serial, sensor_id integer, data float [], time timestamp, constraint fk_sensor foreign key(sensor_id) references sensor(sensor_id));
 create table device (device_id serial, name text, pin integer, description text null, primary key(device_id));
 
-insert into sensor(name, pin, value_type) values ('DHT22', 4, ARRAY ['C', '%']);
-insert into sensor(name, pin, value_type) values ('ultrasonic', 17, ARRAY ['cm']);
-insert into sensor(name, pin, value_type) values ('Photoresistor', 22, ARRAY ['boolean']);
+insert into sensor(name, pin, value_type) values ('ValuesTest', 4, ARRAY ['C', '%', 'distance', 'pressure']);
+insert into sensor(name, pin, value_type) values ('ValuesTest2', 17, ARRAY ['cm', '%']);
 
 insert into device(name, pin) values ('red_led', 26);
